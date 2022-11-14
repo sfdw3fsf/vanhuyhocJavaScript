b34cf86cc8f175f3c2731dd0f4d2ef71f3366657
VawnHuy <huy820op@gmail.com>
2022-11-14 11:09:41 +0700

    template literal/backticks

---
+++ learning/script.js
@@ -0,0 +1,18 @@
+const firstName = 'Huy'
+const job ='student'
+const birthYear =2002;
+const now =2022
+const huy = "I'm "+ firstName + ', a ' 
++ (now-birthYear) +' years old '+job
+//Javascript auto coercion number to string
+//If want to display ' wrap it in ""
+console.log(huy);
+
+const huyNew = `I'm ${firstName}, a ${now-birthYear} years old ${job}`
+//template literal string
+//backticks
+//many developers use backticks for all strings
+const st =`this is a normal string`
+
+console.log(huyNew);
+console.log(st);
