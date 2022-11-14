c8cac7d8730d4135524ccc939d5f726d1b5f7c8e
VawnHuy <huy820op@gmail.com>
2022-11-15 00:20:26 +0700

    Condition

---
+++ learning/Condition/script.js
@@ -0,0 +1,19 @@
+console.log('😊')
+//window + .
+const age =16;
+if(age>18){
+    console.log('Van Huy is old huhu😒');
+} else{
+    leftYear = 18-age;
+    console.log(`Van Huy still have ${leftYear} year to be 18 `)
+}
+
+let year = 2022
+let century;
+if(year>2000){
+    century = '20th century';
+}
+else{
+    console.log('19th century')
+}
+console.log(century)
