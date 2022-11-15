d092b34b11023776f80afdd5d720338f4fbdb74a
VawnHuy <huy820op@gmail.com>
2022-11-15 14:07:55 +0700

    TypeConversion/Coersion

---
+++ learning/TypeConversionCoersion/script.js
@@ -0,0 +1,20 @@
+
+//type conversion
+const name ='huy';
+console.log(Number(name));
+console.log(typeof NaN);
+console.log(String(23))
+
+//type coercion
+console.log("I'm a " + 20 + " years old man")
+//plus trigger coercion number->string
+console.log('22'+2)
+console.log('22'-2)
+//minus is opposite with plus string -> number
+
+
+
+
+//Nan: Not a number
+//manually: thu cong
+//straightforward: thang than
