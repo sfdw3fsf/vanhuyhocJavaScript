cef5f938aed7811b6564ef941811b062dca2e1e9
VawnHuy <huy820op@gmail.com>
2022-11-20 13:51:24 +0700

    Expression and Statments

---
+++ learning/Expression&Statements/script.js
@@ -0,0 +1,12 @@
+// produce a value => expression, EX: 3+4,5657, true && false
+2022-2002
+
+if(2>1){
+    console.log('yes')
+}
+
+console.log(`I'm ${if(2>1){
+    console.log('yes')
+}} years old`)
+// can put expression in template literal but 
+//statement can not
