246591eb34e70d97725f345566a10d357edec8bc
VawnHuy <huy820op@gmail.com>
2022-11-27 09:53:27 +0700

    Finish Modal

---
+++ Modal/script.js
@@ -0,0 +1,31 @@
+'use strict';
+const show = document.querySelectorAll('.show-modal');
+const close = document.querySelector('.close-modal');
+const hidden = document.querySelector('.hidden');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+console.log(show);
+
+for (let i = 0; i < show.length; i++) {
+  show[i].addEventListener('click', openModal)
+}
+
+close.addEventListener('click', closeModal)
+overlay.addEventListener('click', closeModal)
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape') {
+    if (!modal.classList.contains('hidden')) {
+      closeModal();
+    }
+  }
+})
